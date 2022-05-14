37fe0f79ea2bb7ffe85ea71b8be76895c716df61
‘fabioalvaro’ <fabio.alvaro@gmail.com>
2022-05-14 08:42:03 -0300

    adicionado mais testes unitarios ao projeto correcao do chapeuzinho :(

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,58 +1,42 @@
-import { TestBed } from '@angular/core/testing';
-
 import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
+
   let service: UniqueIdService = null;
 
   beforeEach(() => {
     service = new UniqueIdService();
   });
 
-  it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
-  DEVE gerar um id QUANDO informado um prefixo`, () => {
-    const id = service.generateUniqueIdWithPrefix('batata');
-
-    expect(true).toBeTrue();
-    expect(true).toBe(true);
-    expect(true).toBeTruthy();
-  });
-
-  it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
-  NAO DEVE gerar um id duplicado 
-   QUANDO for chamado multiplas vezes`, () => {
-    const firstId = service.generateUniqueIdWithPrefix('batata');
-    const secondId = service.generateUniqueIdWithPrefix('batata');
-
-    const ids = new Set();
-    for (let i = 0; i < 1000; i++) {
-      ids.add(service.generateUniqueIdWithPrefix('batata'));
-    }
-
-    // expect( tatntantana ).toBeTrue();
-
-    //expect(firstId).not.toBe(secondId);
-    expect(ids.size).toBe(1000);
-  });
-
-  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} 
-   DEVE retornar o numero de ids 
-  QUANDO for chamado`, () => {
-    const firstId = service.generateUniqueIdWithPrefix('batata');
-    const secondId = service.generateUniqueIdWithPrefix('batata');
-
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    should return the number of generatedIds when called`, () => {
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
     expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
   });
 
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} 
-   DEVE retornar um erro  
-   QUANDO for chamado sem prefixo/vazio`, () => {
-    const emptyValues = [null, undefined, '', '0', '1'];
-
-    emptyValues.forEach((emptyValue) => {
-      expect(() => service.generateUniqueIdWithPrefix(emptyValue))
-        .withContext(`Empty value:  ${emptyValue}`)
-        .toThrow();
-    });
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should generate id when called with prefix`, () => {
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id.startsWith('app-')).toBeTrue();
   });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should not generate duplicate IDs when called multiple times`, () => {
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+    should throw when called with empty`, () => {
+      const emptyValues = [null, undefined, '', '0', '1'];
+      emptyValues.forEach(emptyValue => {
+        expect(() => service.generateUniqueIdWithPrefix(emptyValue))
+          .withContext(`Empty value: ${emptyValue}`)
+          .toThrow();
+      });
+    });
 });
--- src/app/shared/services/unique-id/unique-id.service.ts
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { v4 as uuidv4 } from 'uuid';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class UniqueIdService {
+
   private numberOfGeneratedIds = 0;
 
-  private validId = /ˆ[A-Za-z]+[\w\-\:\.]*$/;
+  private validId = /^[A-Za-z]+[\w\-\:\.]*$/;
 
   public generateUniqueIdWithPrefix(prefix: string): string {
     if (!prefix || !this.validId.test(prefix)) {
-      throw Error('Prefixo nao pode ser vazio');
+      throw Error('Prefix can not be empty');
     }
     const uniqueId = this.generateUniqueId();
     this.numberOfGeneratedIds++;
