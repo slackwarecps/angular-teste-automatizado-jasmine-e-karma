ba71a6b77ac0b3f83f7e39b23722a5002602b86e
‘fabioalvaro’ <fabio.alvaro@gmail.com>
2022-05-15 10:36:41 -0300

    usando o spy para monitorar outputs

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,24 +1,50 @@
+import { LikeWidgetModule } from './like-widget.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UniqueIdService } from './../../services/unique-id/unique-id.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LikeWidgetComponent } from './like-widget.component';
+import { componentFactoryName } from '@angular/compiler';
 
 describe(LikeWidgetComponent.name, () => {
   let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LikeWidgetComponent],
-      providers: [UniqueIdService],
-      imports: [FontAwesomeModule],
+      imports: [LikeWidgetModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LikeWidgetComponent);
   });
 
-  it(`DEVE criando um componente
+  it(`DEVE criar um componente
     QUANDO inicia `, () => {
-    const instance = fixture.componentInstance;
-    expect(instance).toBeTruthy();
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  });
+
+  it(`DEVE criar um auto id 
+    QUANDO nao for informado um id`, () => {
+    const component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy();
+  });
+
+  it(`DEVE NAO criar um auto id 
+  QUANDO SIM for informado um id`, () => {
+    const component = fixture.componentInstance;
+    const someId = 'alguma-coisa';
+    component.id = someId;
+    fixture.detectChanges();
+    expect(component.id).toBe(someId);
+  });
+
+  it(`#${LikeWidgetComponent.prototype.like.name}  
+      DEVE trigar um evento 
+      QUANDO for chamado`, () => {
+    fixture.detectChanges();
+    const component = fixture.componentInstance;
+    spyOn(component.liked, 'emit');
+    component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
   });
 });
